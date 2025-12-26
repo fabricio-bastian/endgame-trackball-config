2e2b812c4a2c54991d1359a8cbeb598cace81841
Fabricio Bastian <fabricio.bastian@maersk.com>
2025-12-26 13:12:55 +0100

    Squashed commit of the following:
    commit 37f0d2e694ff6113514f777026a694672e0c69d0
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Fri Dec 26 06:11:22 2025 +0200
    
        chore: refactoring
    
    commit e73ccd4519a8327b4ab40fb92d1d5121637671bf
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Fri Dec 26 06:09:56 2025 +0200
    
        bugfix: apply rate limit
    
    commit 62e455f8f77c40cce2c9fd8717a5d791b6075b3c
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Thu Dec 25 03:33:26 2025 +0200
    
        chore: refactoring
    
    commit 226e08f7f497b827c072cf27c0527009cb5e7a8c
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Thu Dec 25 03:33:08 2025 +0200
    
        chore: refactoring
    
    commit 34762b8a17420f5b0d99f3db74ca91b0a1e16594
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Wed Dec 24 04:22:27 2025 +0200
    
        feat: huge power consumption optimization (PM for UART)
    
    commit fcc8bd50e010df85243186c449e407897fa96f64
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Sat Dec 20 16:04:00 2025 +0200
    
        bugfix: enable shell commands
    
    commit 4bf5a9feeb646af3a7576d5f33ffa47b28fbad25
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Sat Dec 20 14:24:44 2025 +0200
    
        chore: defaults
    
    commit 4c866451c7eae9005cb158412edc35ceac22914f
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Sat Dec 20 12:58:01 2025 +0200
    
        chore: defaults
    
    commit 70df2960a9eb91eb1ad7574beccd318f2908b6a5
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Sat Dec 20 02:19:35 2025 +0200
    
        bugfix: flush logs
    
    commit dd92b4e790a1a6326a0ec2c7b23dcdd9b574a77f
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Fri Dec 19 17:49:47 2025 +0200
    
        feat: shell integration (west)
    
    commit 0f3dc2974a989c235c39fdfe86eee167beea2f00
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Fri Dec 19 17:48:41 2025 +0200
    
        feat: shell integration
    
    commit 4003b94ef58349ec28955e404335f8427b4f0139
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Fri Dec 12 15:36:19 2025 +0200
    
        feat: 12x divisor for twist scroll, disable axis clamper
    
    commit 4ffcd7ca803c6296efc6ac1d848d7e876a442597
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Sun Nov 23 16:57:43 2025 +0200
    
        feat: twist scroll toggle behavior
    
    commit cd962e53da03881eb734e4c31046023b037eb5bf
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Fri Nov 21 22:10:56 2025 +0200
    
        feat: twist scroll acceleration
    
    commit 4fcbf15d50c272f1ee66f0e393b82354dca2e5a8
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Fri Nov 14 20:21:51 2025 +0200
    
        chore: refactoring
    
    commit 86153107aacff429f71924ba99c9ae1d23abc47e
    Author: Artem <7541981+efogdev@users.noreply.github.com>
    Date:   Thu Nov 6 10:19:47 2025 +0200
    
        bugfix: hotfix
    
    commit 20d729a46206a0655a642ae59d30e205a69128b2
    Author: Artem <7541981+efogdev@users.noreply.github.com>
    Date:   Thu Nov 6 10:19:34 2025 +0200
    
        bugfix: hotfix
    
    commit de5413e2070519df5f840b7f47600a966eef2648
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Thu Nov 6 02:57:30 2025 +0200
    
        chore: refactoring
    
    commit a4220a374b1468e161570fdca9d56b22b660d96d
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Thu Nov 6 02:35:13 2025 +0200
    
        feat: configure encoders with ZMK Studio
    
    commit ba2b5ed49377ad3090edea4cdf42fa88648f0fac
    Author: Artem Suprun <efogdev@gmail.com>
    Date:   Wed Nov 5 23:58:00 2025 +0200
    
        feat: better physical layout

---
+++ boards/arm/efogtech_trackball_0/efogtech_trackball_0.c
@@ -0,0 +1,96 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/init.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/sys_io.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/shell/shell.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/settings/settings.h>
+#include <zmk/events/usb_conn_state_changed.h>
+#include <zmk/event_manager.h>
+#include <zephyr/device.h>
+#include <zephyr/pm/device.h>
+#include "zmk/endpoints.h"
+
+#if IS_ENABLED(CONFIG_SHELL)
+#define shprint(_sh, _fmt, ...) \
+do { \
+    if ((_sh) != NULL) \
+        shell_print((_sh), _fmt, ##__VA_ARGS__); \
+} while (0)
+
+static int cmd_output(const struct shell *sh, const size_t argc, char **argv) {
+    if (argc < 1) {
+        shprint(sh, "Usage: board output [usb|ble]");
+        return -EINVAL;
+    }
+
+    if (strcmp(argv[1], "usb") == 0) {
+        zmk_endpoints_select_transport(ZMK_TRANSPORT_USB);
+    } else if (strcmp(argv[1], "ble") == 0) {
+        zmk_endpoints_select_transport(ZMK_TRANSPORT_BLE);
+    } else {
+        if (zmk_endpoints_selected().transport == ZMK_TRANSPORT_USB) {
+            shprint(sh, "Output: USB");
+        } else {
+            shprint(sh, "Output: BLE");
+        }
+
+        return 0;
+    }
+
+    shprint(sh, "Done.");
+    return 0;
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(sub_board,
+    SHELL_CMD(output, NULL, "Get or set output channel (USB/BLE)", cmd_output),
+    SHELL_SUBCMD_SET_END
+);
+
+SHELL_CMD_REGISTER(board, &sub_board, "Control the device", NULL);
+#endif
+
+static const struct device *p0 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
+static const struct device *p1 = DEVICE_DT_GET(DT_NODELABEL(gpio1));
+static const struct device *uart = DEVICE_DT_GET(DT_NODELABEL(uart0));
+
+static void set_3v3_en(const bool en) {
+    gpio_pin_configure(p1, 0, GPIO_OUTPUT);
+    gpio_pin_set(p1, 0, en);
+}
+
+static void set_rgb_en(const bool en) {
+    gpio_pin_configure(p1, 3, GPIO_OUTPUT);
+    gpio_pin_set(p1, 3, en);
+}
+
+static void set_bl_en(const bool en) {
+    gpio_pin_configure(p0, 20, GPIO_OUTPUT);
+    gpio_pin_set(p0, 20, en);
+}
+
+static int pinmux_efgtch_trckbl_init(void) {
+    set_3v3_en(false);
+    set_rgb_en(false);
+    set_bl_en(false);
+    return 0;
+}
+
+SYS_INIT(pinmux_efgtch_trckbl_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+
+#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
+static int usb_conn_chg(const zmk_event_t *eh) {
+    if (zmk_usb_get_conn_state() == ZMK_USB_CONN_HID) {
+        pm_device_action_run(uart, PM_DEVICE_ACTION_RESUME);
+    } else {
+        pm_device_action_run(uart, PM_DEVICE_ACTION_SUSPEND);
+    }
+
+    return 0;
+}
+
+ZMK_SUBSCRIPTION(board_root, zmk_usb_conn_state_changed);
+ZMK_LISTENER(board_root, usb_conn_chg)
+#endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
